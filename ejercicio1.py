3ad227d5491676e6d6debe74e7afd23e78afe8b0
Rodrigo Mateos <114070557+rodrigomateosv@users.noreply.github.com>
2022-12-11 21:36:13 +0000

    3

--- ejercicio1.py
+++ ejercicio1.py
@@ -8,4 +8,14 @@ def main():
  # Cargar como dataframe de pandas el csv imdb_elenco.csv y mostrar sus 5 primeros registros
     elenco = pd.read_csv('imdb_elenco.csv')
     print("Cargando el archivo imdb_elenco.csv...")
+print(elenco.head()) #head() devuelve los 5 primeros registros del dataframe por defecto pero es modificable mediante el parámetro "n"
+    print("\n")
+
+    #Mostrar el número de registros del dataframe de titulos
+    print("Mostrar el número de registros del dataframe de titulos: {}".format(len(titulos_peliculas)), "\n", ) #len() devuelve el número de registros del dataframe.
+
+
+    #Mostrar el número de registros del dataframe de elenco
+    print("Mostrar el número de registros del dataframe de elenco: {}.".format(len(elenco)), "\n") #len() devuelve el número de registros del dataframe.
+
 
