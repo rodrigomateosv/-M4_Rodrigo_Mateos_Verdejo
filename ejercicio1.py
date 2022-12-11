89b3ce737b1575fa1408f531b39c8d057e48fc8b
Rodrigo Mateos <114070557+rodrigomateosv@users.noreply.github.com>
2022-12-11 21:35:27 +0000

    2

--- ejercicio1.py
+++ ejercicio1.py
@@ -3,3 +3,9 @@ import pandas as pd  # Importa pandas y usa un alias para poder utilizar sus rec
 def main():
     # Cargar como dataframe de pandas el csv imdb_titulos.csv y mostrar sus 5 primeros registros
     titulos_peliculas = pd.read_csv('imdb_titulos.csv')
+  print("Cargando el archivo imdb_titulos.csv...")
+    print(titulos_peliculas.head()) #head() devuelve los 5 primeros registros del dataframe
+ # Cargar como dataframe de pandas el csv imdb_elenco.csv y mostrar sus 5 primeros registros
+    elenco = pd.read_csv('imdb_elenco.csv')
+    print("Cargando el archivo imdb_elenco.csv...")
+
