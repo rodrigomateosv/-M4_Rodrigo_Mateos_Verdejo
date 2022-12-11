921ffbcbf8888e2fab69a18aa5da7133d324eadc
Rodrigo Mateos <114070557+rodrigomateosv@users.noreply.github.com>
2022-12-11 21:34:18 +0000

    1

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,1 +1,5 @@
 import pandas as pd  # Importa pandas y usa un alias para poder utilizar sus recursos
+
+def main():
+    # Cargar como dataframe de pandas el csv imdb_titulos.csv y mostrar sus 5 primeros registros
+    titulos_peliculas = pd.read_csv('imdb_titulos.csv')
